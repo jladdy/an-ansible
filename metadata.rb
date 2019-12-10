96712e2c0fec62e2ecbb97644b3d97afa94574e6
Jeremy Addy <addynetops@gmail.com>
2019-12-10 18:52:29 +0000

    Initiall commit

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'an-ansible'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer 'AddyNet Operations'
+maintainer_email 'addynetops@gmail.com'
 license 'All Rights Reserved'
 description 'Installs/Configures an-ansible'
 long_description 'Installs/Configures an-ansible'
@@ -11,10 +11,10 @@ chef_version '>= 14.0'
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/an-ansible/issues'
+# issues_url 'https://github.com/jladdy/an-ansible/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/an-ansible'
+# source_url 'https://github.com/jladdy/an-ansible'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,16 @@
 # Cookbook:: an-ansible
 # Recipe:: default
 #
-# Copyright:: 2019, The Authors, All Rights Reserved.
+# Copyright:: 2019, AddyNet Operations, All Rights Reserved.
+
+apt_repository 'ansible' do
+  uri        'http://ppa:ansible/ansible'
+end
+
+package 'ansible' do
+        action :install 
+end
+
+package 'python' do
+	action :install 
+end
